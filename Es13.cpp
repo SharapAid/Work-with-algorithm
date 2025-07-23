e6ceb4c3c831398492928a4a8afb3b13082edc5e
Viktoria <vikachrnosej@gmail.com>
2025-07-23 17:36:01 +0200

    Add new func in list

--- Es13.cpp
+++ Es13.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-template<class T>
+template<typename T>
+class DoublyLinkedList;
+
+template<typename T>
+void Print(DoublyLinkedList<T>& list);
+
+template<typename T>
 class DoublyLinkedList{
 private:
     struct Node{
@@ -12,6 +18,9 @@ private:
     };
     Node* head;
     Node* tail;
+
+    Node* merge(Node* first_half, Node* second_half);
+    Node* split(Node* head_ref);
 
 public:
     DoublyLinkedList(): head(nullptr), tail(nullptr){}
@@ -26,6 +35,11 @@ public:
             head = nullptr;
         }
     }
+    Node* GetHead(){
+        return head;
+    }
+
+    friend void Print<T>(DoublyLinkedList<T>& list);
 
     void push_back(T value);
     void push_front(T value);
@@ -36,9 +50,10 @@ public:
     bool is_empty(){
         return head == nullptr;
     }
+    Node* merge_sort(Node* head);
 };
 
-template<class T>
+template<typename T>
 void DoublyLinkedList<T>::push_back(T value){
     Node* new_node = new Node(value);
     if(head == nullptr){
@@ -52,7 +67,7 @@ void DoublyLinkedList<T>::push_back(T value){
     }
 }
 
-template<class T>
+template<typename T>
 void DoublyLinkedList<T>::push_front(T value){
     Node* new_node = new Node(value);
     if(head == nullptr){
@@ -61,12 +76,12 @@ void DoublyLinkedList<T>::push_front(T value){
     }
     else{
         new_node->next = head;
-        head->prev = new_node;
+        head->pred = new_node;
         head = new_node;  
     }
 }
 
-template<class T>
+template<typename T>
 void DoublyLinkedList<T>::pop_front(){
     if(head != nullptr){
         
@@ -79,13 +94,13 @@ void DoublyLinkedList<T>::pop_front(){
         }
         else{
             head = head->next;
-            head->prev = nullptr;
+            head->pred = nullptr;
             delete temp;
         }
     }
 }
 
-template<class T>
+template<typename T>
 void DoublyLinkedList<T>::pop_back(){
     if(tail != nullptr){
         if(tail == head){
@@ -102,9 +117,8 @@ void DoublyLinkedList<T>::pop_back(){
     }
 }
 
-
-template<class T>
-DoublyLinkedList<T>::Node* DoublyLinkedList<T>::find(T value){
+template<typename T>
+typename DoublyLinkedList<T>::Node* DoublyLinkedList<T>::find(T value){
     Node* current_node = head;
     while(current_node != nullptr && current_node->m_value != value){
         current_node = current_node->next;
@@ -112,7 +126,7 @@ DoublyLinkedList<T>::Node* DoublyLinkedList<T>::find(T value){
     return current_node;
 }
 
-template<class T>
+template<typename T>
 void DoublyLinkedList<T>::sort(){
     if(head != nullptr){
         do{
@@ -138,6 +152,105 @@ void DoublyLinkedList<T>::sort(){
     }
 }
 
+template<typename T>
+typename DoublyLinkedList<T>::Node* DoublyLinkedList<T>::split(Node* head_ref){
+    if(head_ref != nullptr && head_ref->next != nullptr){
+        Node* slow = head_ref;
+        Node* fast = head_ref;
+        Node* prev_slow = nullptr;
+        while(fast && fast->next != nullptr){
+            prev_slow = slow;
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        Node* second_half_head = slow;
+        prev_slow->next = nullptr;
+        second_half_head->pred = nullptr; 
+        return second_half_head;
+    }
+    return head_ref;
+}
+
+template<typename T>
+typename DoublyLinkedList<T>::Node*  DoublyLinkedList<T>::merge(Node* first_half, Node* second_half){
+    if(first_half == nullptr){
+        return second_half;
+    }
+
+    if(second_half == nullptr){
+        return first_half;
+    }
+
+    Node* first = first_half;
+    Node* second = second_half;
+    Node* dummy = new Node(T());
+    Node* current = dummy;
+    while(first != nullptr && second != nullptr){
+        if(first->m_value < second->m_value){
+            current->next = first;
+            first->pred = current;
+            first = first->next;
+        }
+        else{
+            current->next = second;
+            second->pred = current;
+            second = second->next;
+        }
+        current = current->next;
+    }
+
+    if(first != nullptr){
+        current->next = first;
+        first->pred = current;
+    } 
+
+    else if(second != nullptr){
+        current->next = second;
+        second->pred = current;
+    } 
+    
+    Node* new_head = dummy->next;
+    new_head->pred = nullptr;
+    delete dummy;
+    dummy = nullptr;
+    second = nullptr;
+    first = nullptr;
+    current = nullptr;
+    return new_head;
+}
+
+template<typename T>
+typename DoublyLinkedList<T>::Node* DoublyLinkedList<T>::merge_sort(Node* head){
+    if(head == nullptr || head->next == nullptr){
+        return head;
+    }
+    Node* first_half_head = head;
+    Node* second_half_head = split(head);
+    Node* first_half_sorted = merge_sort(first_half_head);
+    Node* second_half_sorted = merge_sort(second_half_head);
+    return merge(first_half_sorted, second_half_sorted);
+}
+
+template<typename T>
+void Print(DoublyLinkedList<T>& list){
+    typename DoublyLinkedList<T>::Node* current_node = list.GetHead();
+    while(current_node != nullptr){
+        std::cout<<current_node->m_value<<'\t';
+        current_node = current_node->next;
+    }
+    std::cout<<'\n';
+}
+
 int main(){
+    DoublyLinkedList<int> list;
+    list.push_front(3);
+    list.push_front(12);
+    list.push_front(7);
+    list.push_front(35);
+    list.push_front(17);
+    list.push_front(1);
+    Print(list);
+    list.merge_sort(list.GetHead());
+    Print(list);
     return 0;
 }
